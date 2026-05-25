c8ba5b2cbaf8890a47547624cf5deb4bcac6a663
Ale-2708 <bianchinialessandro081@gmail.com>
2026-05-25 22:57:56 +0200

    Add files via upload

--- FSL/Esercizio finale JS/script.js
+++ FSL/Esercizio finale JS/script.js
@@ -1,5 +1,10 @@
-async function insertUser(pagina){
-    let URL = `https://dummyjson.com/users?limit=20&skip=${pagina * 20}&select=firstName,lastName,gender,age,height,weight,eyeColor,hair`
+async function insertUser(pagina, filtro){
+    if(pagina == undefined){
+        let a = document.getElementsByClassName("actual-page")[0]
+        pagina = parseInt(a.id[a.id.length -1] ,10) 
+    }
+    
+    let URL = filtro != undefined ? `https://dummyjson.com/users?sortBy=${filtro}&order=asc&limit=20&skip=${(pagina-1) * 20}&select=lastName,firstName,gender,age,height,weight,eyeColor,hair` : `https://dummyjson.com/users?limit=20&skip=${(pagina-1) * 20}&select=firstName,lastName,gender,age,height,weight,eyeColor,hair`
     let result = await fetch(URL)
     let json = await result.json()
     let utenti = json.users
@@ -53,4 +58,47 @@ function research(spostamento){
     }
 }
 
+async function filter(event) {
+    event.preventDefault();
+    let input = document.getElementsByTagName("input")[0]
+    let URL = `https://dummyjson.com/users/search?q=${input.value}`
+    let result = await fetch(URL)
+    let json = await result.json()
+    let utenti = json.users
+
+    let tbody = document.getElementsByTagName("tbody")[0]
+    tbody.innerHTML = ""
+
+    utenti.forEach(element => {
+        tbody.innerHTML += `<tr>
+                    <td>${element.lastName}</td>
+                    <td>${element.firstName}</td>
+                    <td>${element.gender[0].toUpperCase()}</td>
+                    <td>${element.age}</td>
+                    <td>${element.height}</td>
+                    <td>${element.weight}</td>
+                    <td>${element.eyeColor}</td>
+                    <td>${element.hair.type} ${element.hair.color}</td>
+        </tr>
+        `
+    });
+}
+
 insertUser(1)
+
+let span = document.getElementsByClassName("span-filter")
+
+for (const element of span) {
+    element.addEventListener('click', (e) => {
+        insertUser(undefined, e.target.id);
+    });
+}
+
+/*Cose che non so fare:
+    aggiugere l'event listener (questo sopra)
+    fare in modo di salvare il filtro per tutte le pagine 
+    fare in modo che passi da crescente a decrescente e che poi il filtro si resetti 
+    salvare il decrescente tra una pagina e un altra senza una variabile globale
+    non so se è giusto il filtro con l'input che lascia solo alcuni risultati con search
+
+*/
